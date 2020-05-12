ce41ecbf1a8ceff6d0e0132210e274e4dea80e58
quanglinh2210 <61451590+quanglinh2210@users.noreply.github.com>
2020-05-12 13:40:07 +0700

    Add files via upload

---
+++ btth5.1 (1).py
@@ -0,0 +1,13 @@
+## file : mymarth.py ##
+def square(n):
+    return n*n
+
+def cube(n):
+    return n*n*n
+
+def average(values):
+    nvals = len(values)
+    sum = 0.0
+    for v in values:
+        sum += V
+    return float(sum)/nvals
---
+++ btth6.1.py
@@ -0,0 +1,9 @@
+class crircle(object):
+    def _init_(self,r):
+        self.radius = r
+###############################
+    def area(self):
+        return self.radius**2*3,14
+
+aCircle = Circle(2)
+print (aCircle.area())
